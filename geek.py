f8b3b982df520318b21fff9f6963a9d4b12d5834
RafaelFSFarias <135792637+RafaelFSFarias@users.noreply.github.com>
2023-06-07 01:46:42 -0300

    geek.py

---
+++ geek.py
@@ -0,0 +1,8 @@
+print('Bem vindo ao geek')
+
+nome = input('Insira seu nome: ')
+cpf = input('Insira seu cpf: ')
+email = input('Insira seu email: ')
+modalidade = input('Insira a modalidade que deseja praticipar: ')
+
+print('Participante: {}\ncpf: {}\nemail: {}\nmodalidade: {}\n '.format(nome, cpf, email, modalidade))
